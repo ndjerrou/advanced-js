5025d4e2312fc35759d476bfeac7f2692cb43f76
ndjerrou <n.djerrou@gmail.com>
2021-12-01 11:08:27 +0100

    Added tags to the homepage - Photographers exo

--- classes/navTags.js
+++ classes/navTags.js
@@ -1,9 +1,46 @@
 import data from "../data.json";
 
-console.log(data);
+const tagsPhotographers = [];
 
-const objTags = {};
-const tags = ["sports", "architecture"];
-tags.forEach((tag) => {
-  objTags[tag] = tag;
+// [tags] ==> {tag1: ..., tag2: ..., }
+const createUniqueTags = (tags) => {
+  const uniqueTags = {};
+
+  tags.forEach((tag) => {
+    uniqueTags[tag] = 1;
+  });
+
+  return uniqueTags;
+};
+
+// display into the dom an array of tags into a specific node
+const dispTags = (tags, node) => {
+  node.innerHTML = "";
+  tags.forEach((tag) => {
+    node.innerHTML += `<li>#${tag}</li>`;
+  });
+};
+
+// alternative to dispTags
+const dispTags1 = (tag, node) => {
+  node.innerHTML += `<li>#${tag}</li>`;
+};
+
+// fulfill our tagsPhotographers array with tags
+data.photographers.forEach((photographer) => {
+  photographer.tags.forEach((tag) => {
+    tagsPhotographers.push(tag);
+  });
 });
+
+// Creating an array with the DISCTING tags
+const uniqueTagsObj = createUniqueTags(tagsPhotographers);
+const uniqueTagsArray = Object.keys(uniqueTagsObj);
+
+dispTags(uniqueTagsArray, document.querySelector("ul"));
+
+// or ...
+
+// for (let tag in uniqueTagsObj) {
+//   dispTags1(tag, document.querySelector("ul"));
+// }
